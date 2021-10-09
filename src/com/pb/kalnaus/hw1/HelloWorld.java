d7f2082da4801317735ba0e50ce275cea0c0b375
Jurij Kalnaus <jurij.kalnaus@privatbank.ua>
2021-10-10 01:51:14 +0300

    first init commit

--- src/com/pb/kalnaus/hw1/HelloWorld.java
+++ src/com/pb/kalnaus/hw1/HelloWorld.java
@@ -1,7 +1,5 @@
 package com.pb.kalnaus.hw1;
 
-import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
-
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
