20b3fe189e581820ea7cc687bc0328f28386a1f4
Jurij Kalnaus <jurij.kalnaus@privatbank.ua>
2021-12-13 23:51:41 +0200

    hw10----

---
+++ src/com/pb/kalnaus/hw11/Main.java
@@ -0,0 +1,348 @@
+package com.pb.kalnaus.hw11;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+
+import java.io.*;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.util.*;
+
+public class Main {
+
+
+    static Scanner in = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+
+        List<PhoneBooks> phoneBooks = new ArrayList<>();
+        String action = "";
+
+        ObjectMapper mappers = new ObjectMapper();
+        mappers.enable(SerializationFeature.INDENT_OUTPUT);
+        SimpleModule modules = new SimpleModule();
+        modules.addSerializer(LocalDate.class, new LocalDateSerializer());
+        modules.addDeserializer(LocalDate.class, new LocalDateDeserializer());
+        mappers.registerModule(modules);
+
+        File file = new File("C:\\Users\\PC\\IdeaProjects\\JavaHomeWork\\src\\com\\pb\\kalnaus\\hw11\\files\\PhoneBook.json");
+
+
+        System.out.println("Программа Телефонная книга. Может добавлять, удалять, делать поиск, вывод всех записей" +
+                ",сортировкой по полю, редактированием элемента. Записывает все в файл и считывает все из него");
+        startMessage();
+        while (!action.equals("exit")) {
+            action = in.nextLine();
+            switch (action.toLowerCase().trim()) {
+
+                case "add":
+                    addElem(phoneBooks);
+                    break;
+
+                case "del":
+                    if (phoneBooks.size() == 0) {
+                        System.out.println("Массив пустой, нечего удалять ");
+                        break;
+                    }
+                    dellElem(phoneBooks);
+                    break;
+
+                case "search":
+                    if (phoneBooks.isEmpty()) {
+                        System.out.println("Нечего искать. Массив пустой");
+                    } else {
+                        searchElem(phoneBooks);
+                    }
+                    break;
+
+                case "print":
+                    // вывод всех записей с сортировкой по указанному полю (можно ограничиться двумя на выбор)
+                    if (phoneBooks.size() == 0) {
+                        System.out.println("Нечего выводить, Массив пустой");
+                    } else {
+                        printCollection(phoneBooks);
+                    }
+                    break;
+                case "edit":
+                    //3 редактирование элемента edit
+                    if (phoneBooks.size() == 0) {
+                        System.out.println("Нечего редактировать, Массив пустой");
+                    } else {
+                        editNumberCollection(phoneBooks);
+                    }
+                    break;
+
+                case "save":
+                    mappers.writeValue(new File(String.valueOf(file)), phoneBooks);
+                    System.out.println("Данные успешно сохранены в файл");
+                    break;
+
+
+                case "load":
+                    phoneBooks.clear();
+                    phoneBooks = mappers.readValue(new File(String.valueOf(file)), new TypeReference<List<PhoneBooks>>() {
+                    });
+                    System.out.println(phoneBooks.get(0).getClass().getName());
+                    System.out.println(phoneBooks);
+
+                    break;
+
+                case "exit":
+                    System.out.println("Программа завершила работу.");
+                    break;
+
+                default:
+                    System.out.println("Вы ввели неверное значение, повторите попытку");
+                    break;
+            }
+        }
+    }
+
+    private static void editNumberCollection(List<PhoneBooks> phoneBooks) {
+        System.out.println("Для редактирования, укажите номер строки:");
+        int sizeBooks = 0;
+        int input;
+        ListIterator<PhoneBooks> phoneBooksListIterator = phoneBooks.listIterator();
+        while (phoneBooksListIterator.hasNext()) {
+            System.out.println(sizeBooks + " " + phoneBooksListIterator.next());
+            sizeBooks++;
+        }
+        try {
+            input = Integer.valueOf(in.nextLine());
+            if (input < phoneBooks.size()) {
+                editElem(input, phoneBooks);
+            }
+        } catch (NumberFormatException e) {
+            System.out.println("Вы ввели неверное значение, повторите попытку");
+        }
+
+
+    }
+
+    private static void editElem(int editNumCollection, List<PhoneBooks> phoneBooks) {
+        String tempLine;
+        String tempChange;
+        int[] tempPhonesNumMass;
+        System.out.println("Выберите, какое поле редактируем:");
+        System.out.println("1 - ФИО:");
+        System.out.println("2 - дата рождения(формат: yyyy-MM-dd");
+        System.out.println("3 - номера телефонов:");
+        System.out.println("4 - адресс");
+        tempLine = in.nextLine();
+        System.out.println("Вы выбрали поле: " + tempLine + ". Введите значение для замены:");
+        try {
+            tempChange = in.nextLine();
+            switch (Integer.valueOf(tempLine)) {
+                case 1: //ФИО
+                    phoneBooks.get(editNumCollection).setF_I_O(tempChange);
+                    successfullReplaced(editNumCollection, phoneBooks);
+
+                    break;
+                case 2: // Дата рождения
+                    phoneBooks.get(editNumCollection).setB_Day(LocalDate.parse(tempChange.replaceAll("[^\\d-]", ""), DateTimeFormatter.ofPattern("yyyy-MM-dd")));
+                    successfullReplaced(editNumCollection, phoneBooks);
+                    break;
+                case 3: // Номера телефонов)
+                    tempPhonesNumMass = Arrays.stream(tempChange.replaceAll("[^\\d,]", "").split(","))
+                            .mapToInt(Integer::parseInt).toArray();
+                    phoneBooks.get(editNumCollection).setPhones(tempPhonesNumMass);
+                    successfullReplaced(editNumCollection, phoneBooks);
+                    break;
+                case 4: //адресс
+                    phoneBooks.get(editNumCollection).setAddress(tempChange);
+                    successfullReplaced(editNumCollection, phoneBooks);
+                    break;
+
+                default:
+                    System.out.println("Вы ввели неверное значение, повторите попытку");
+                    startMessage();
+                    break;
+            }
+        } catch (DateTimeParseException e) {
+            System.out.println("Вы указали неверные данные при вводе даты, повторите все сначала");
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Вы ввели неверное значение, повторите попытку");
+        }
+
+    }
+
+    private static void successfullReplaced(int editNumCollection, List<PhoneBooks> phoneBooks) {
+        phoneBooks.get(editNumCollection).setDate_edit(new SimpleDateFormat("yyyy-MM-dd_HH:mm:ss").format(Calendar.getInstance().getTime()));
+        System.out.println("Данные успешно изменены");
+    }
+
+
+    private static void printCollection(List<PhoneBooks> phoneBooks) {
+        int temp;
+        System.out.println("Для вывода отсортированного поля, укажите, по какому полю сортируем:");
+        System.out.println(" \"1\" - По ФИО");
+        System.out.println(" \"2\" - По дате рождения");
+        System.out.println(" \"3\" - По ФИО и дате рождения");
+        System.out.println(" \"4\" - По номерам телефонов");
+        System.out.println(" \"5\" - По адресам");
+        temp = Integer.valueOf(in.nextLine());
+        sortCollection(temp, phoneBooks);
+
+    }
+
+    private static void sortCollection(int temp, List<PhoneBooks> phoneBooks) {
+
+        switch (temp) {
+            //По ФИО
+            case 1:
+                Collections.sort(phoneBooks, new PhoneBooks.PhoneBooksFIOCompare());
+                System.out.println(phoneBooks);
+
+
+                break;
+            case 2:
+                //   По дате рождения
+                Collections.sort(phoneBooks, new PhoneBooks.PhoneBooks_B_DAY_Compare());
+                System.out.println(phoneBooks);
+                break;
+
+            case 3:
+                //По ФИО и дате рождения
+                Collections.sort(phoneBooks, new PhoneBooks.PhoneBooksFIOCompare().thenComparing(new PhoneBooks.PhoneBooks_B_DAY_Compare()));
+                System.out.println(phoneBooks);
+                break;
+            case 4:
+                // По номерам телефонов"
+                Collections.sort(phoneBooks, new PhoneBooks.PhoneBooksPhoneCompare());
+                System.out.println(phoneBooks);
+                break;
+            case 5:
+                // По адресам"
+                Collections.sort(phoneBooks, new PhoneBooks.PhoneBooksAdressCompare());
+                System.out.println(phoneBooks);
+                break;
+            default:
+                System.out.println("Вы ввели неверное значение, повторите попытку");
+                startMessage();
+                break;
+
+        }
+    }
+
+
+    private static void searchElem(List<PhoneBooks> phoneBooks) {
+        int b = 0;
+        System.out.println("Введите тект, который ищете:");
+        String temp = in.nextLine();
+        for (int i = 0; i < phoneBooks.size(); i++) {
+            String massString = phoneBooks.get(i).getF_I_O() + " " +
+                    phoneBooks.get(i).getB_Day() + " " +
+                    Arrays.toString(phoneBooks.get(i).getPhones()) + " " +
+                    phoneBooks.get(i).getAddress() + " " +
+                    phoneBooks.get(i).getDate_edit();
+            if (massString.contains(temp)) {
+                System.out.println("Был знайден элемент в массиве. В элементе: " + i);
+                System.out.println(massString);
+                b++;
+            }
+        }
+        if (b == 0) System.out.println("Совпадений не найдено ");
+
+
+    }
+
+    private static void dellElem(List<PhoneBooks> phoneBooks) {
+        try {
+            int temp = 0;
+            Iterator iter = phoneBooks.listIterator();
+            System.out.println("Выберите номер элемента, который необходимо удалить:");
+            for (int i = 0; i < phoneBooks.size(); i++) {
+                System.out.println("Элемент массива:" + i + iter.next().toString());
+            }
+            temp = Integer.valueOf(in.nextLine());
+            if (temp > phoneBooks.size() - 1) {
+                System.out.println("Вы указали неверное значение для удаления. попробуйте сначала...");
+                startMessage();
+            } else {
+                phoneBooks.remove(temp);
+                System.out.println("Элемент " + temp + " удалили");
+            }
+
+        } catch (ArrayIndexOutOfBoundsException | NumberFormatException e) {
+            System.out.println("Вы указали неверные данные.");
+        }
+    }
+
+    private static void addElem(List<PhoneBooks> list) {
+        String fio;
+        String phones;
+        int[] phonesMass;
+        String address;
+        LocalDate b_day;
+        try {
+            String timeStamp = new SimpleDateFormat("yyyy-MM-dd_HH:mm:ss").format(Calendar.getInstance().getTime());
+            //-----------------------------------------------------------------------//
+            System.out.println("Введите ФИО");
+            fio = in.nextLine();
+            if (fio.length() == 0) {
+                System.out.println("ОШИБКА. Вы не ввели ФИО");
+                startMessage();
+                return;
+            }
+            System.out.println("Введите дату рождения в формате yyyy-MM-dd");
+            b_day = LocalDate.parse(in.nextLine().replaceAll("[^\\d-]", ""), DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+//-----------------------------------------------------------------------//
+            System.out.println("Введите номера телефонов, через запятую");
+            phones = in.nextLine().replaceAll("[^\\d,]", "");
+            phonesMass = Arrays.stream(phones.split(","))
+                    .mapToInt(Integer::parseInt)
+                    .toArray();
+//-----------------------------------------------------------------------//
+            System.out.println("Введите адрес");
+            address = in.nextLine();
+            if (address.length() == 0) {
+                System.out.println("ОШИБКА. Вы не ввели адресс");
+                startMessage();
+                return;
+            }
+//-----------------------------------------------------------------------//
+            System.out.println(" ФИО =" + fio + " день варенья =" + b_day + " телефоный(е) номера =" + Arrays.toString(phonesMass) + " адрес =" + address + " время редактирования =" + timeStamp);
+            list.add(new PhoneBooks(fio, b_day, phonesMass, address, timeStamp));
+            System.out.println("Данные успешно добавленны");
+        } catch (DateTimeParseException e) {
+            System.out.println("Вы ввели неверную дату. Повторите операцию еще раз");
+            startMessage();
+        } catch (NumberFormatException e) {
+            System.out.println("Вы ввели не номер телефона. Попробуйте выполнить действие сначала");
+            startMessage();
+        }
+    }
+ /*  Каждый элемент должен иметь:
+    ФИО
+    дата рождения
+    телефоны (количество не ограничено)
+    адрес
+    дата и время редактирования
+
+    Обеспечить следующий функционал:
+    добавление элемента
+    удаление элемента
+    поиск элементов
+    вывод всех записей с сортировкой по указанному полю (можно ограничиться двумя на выбор)
+    редактирование элемента
+    запись в файл всех данных
+    загрузка из файла всех данных*/
+
+    private static void startMessage() {
+        System.out.println("Для справки: add    - добавить элемент ");
+        System.out.println("             del    - удалить элемент ");
+        System.out.println("             search - поиск элемента ");
+        System.out.println("             print - вывод всех записей с сортировкой по указанному полю (можно ограничиться двумя на выбор) ");
+        System.out.println("             edit - редактировать элемент ");
+        System.out.println("             save - запись элемента в файл ");
+        System.out.println("             load - чтение элементов из файла ");
+        System.out.println("             exit - завершение программы ");
+        System.out.println("Выберите действие, которое вы хотите выполнить: ");
+    }
+
+
+}
---
+++ src/com/pb/kalnaus/hw11/PhoneBooks.java
@@ -0,0 +1,140 @@
+package com.pb.kalnaus.hw11;
+
+
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class PhoneBooks {
+    private String f_I_O;
+    private LocalDate b_Day;
+    private int[] phones;
+    private String address;
+    private String date_edit;
+
+    public PhoneBooks() {
+    }
+
+    public PhoneBooks(String f_I_O, LocalDate b_Day, int[] phones, String address, String date_edit) {
+        this.f_I_O = f_I_O;
+        this.b_Day = b_Day;
+        this.phones = phones;
+        this.address = address;
+        this.date_edit = date_edit;
+    }
+
+    @Override
+    public String toString() {
+        return "\n" + " PhoneBooks{" +
+                "f_I_O='" + f_I_O + '\'' +
+                ", b_Day=" + b_Day +
+                ", phones=" + Arrays.toString(phones) +
+                ", address='" + address + '\'' +
+                ", change time='" + date_edit + '\'' +
+                '}';
+    }
+
+
+    public String getF_I_O() {
+        return f_I_O;
+    }
+
+    public void setF_I_O(String f_I_O) {
+        this.f_I_O = f_I_O;
+    }
+
+    public LocalDate getB_Day() {
+        return b_Day;
+    }
+
+    public void setB_Day(LocalDate b_Day) {
+        this.b_Day = b_Day;
+    }
+
+    public int[] getPhones() {
+        return phones;
+    }
+
+
+    public void setPhones(int[] phones) {
+        this.phones = phones;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getDate_edit() {
+        return date_edit;
+    }
+
+    public void setDate_edit(String date_edit) {
+        this.date_edit = date_edit;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof PhoneBooks)) return false;
+
+        PhoneBooks that = (PhoneBooks) o;
+
+        if (getF_I_O() != null ? !getF_I_O().equals(that.getF_I_O()) : that.getF_I_O() != null) return false;
+        if (getB_Day() != null ? !getB_Day().equals(that.getB_Day()) : that.getB_Day() != null) return false;
+        if (!Arrays.equals(getPhones(), that.getPhones())) return false;
+        if (getAddress() != null ? !getAddress().equals(that.getAddress()) : that.getAddress() != null) return false;
+        return getDate_edit() != null ? getDate_edit().equals(that.getDate_edit()) : that.getDate_edit() == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = getF_I_O() != null ? getF_I_O().hashCode() : 0;
+        result = 31 * result + (getB_Day() != null ? getB_Day().hashCode() : 0);
+        result = 31 * result + Arrays.hashCode(getPhones());
+        result = 31 * result + (getAddress() != null ? getAddress().hashCode() : 0);
+        result = 31 * result + (getDate_edit() != null ? getDate_edit().hashCode() : 0);
+        return result;
+    }
+
+
+    static class PhoneBooks_B_DAY_Compare implements Comparator<PhoneBooks> {
+        public int compare(PhoneBooks a, PhoneBooks b) {
+            return a.getB_Day().compareTo(b.getB_Day());
+        }
+    }
+
+    static class PhoneBooksFIOCompare implements Comparator<PhoneBooks> {
+        public int compare(PhoneBooks a, PhoneBooks b) {
+            return a.getF_I_O().compareTo(b.getF_I_O());
+        }
+
+    }
+
+    static class PhoneBooksAdressCompare implements Comparator<PhoneBooks> {
+        public int compare(PhoneBooks a, PhoneBooks b) {
+            return a.getAddress().compareTo(b.getAddress());
+        }
+    }
+
+
+    static class PhoneBooksPhoneCompare implements Comparator<PhoneBooks> {
+        public int compare(PhoneBooks a, PhoneBooks b) {
+            if (a.getPhones().length > b.getPhones().length)
+                return 1;
+            else if (a.getPhones().length < b.getPhones().length)
+                return -1;
+            else
+                return 0;
+        }
+    }
+
+}
+
+
+
+
+
